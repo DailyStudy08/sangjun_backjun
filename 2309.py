4d7040071a704d26bb7c09ee1d7e9392151acdef
BaeSangjun <qotkdwns1324@naver.com>
2022-07-29 17:19:15 +0900

    1107, 2309

---
+++ 2309.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+from itertools import combinations
+
+result = []
+for _ in range(9):
+    result.append(int(input()))
+
+answer_lst = list(combinations(result, 7))
+answer = []
+for i in answer_lst:
+    if sum(i) == 100:
+        answer = i
+for i in sorted(answer):
+    print(i)
