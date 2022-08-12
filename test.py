e4b134cb55c73f437ffe8a0103bb030a064d4aec
배상준 <qotkdwns1324@naver.com>
2022-08-12 22:56:22 +0900

    2407

--- test.py
+++ test.py
@@ -2,57 +2,31 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-def go(y, x, color, no_color):
+def go(my, mx, color, no_color):
     lst = []
     for i in range(8):
-        for j in range(n):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        nx = mx
+        ny = my
+        for _ in range(n):
+            nx += dx[i]
+            ny += dy[i]
             if 0 <= nx < n and 0 <= ny < n and board[ny][nx] == color:
                 lst.append((ny, nx, i))
                 break
             elif 0 <= nx < n and 0 <= ny < n and board[ny][nx] == no_color:
-                x = nx
-                y = ny
-            else:
+                continue
+            elif 0 <= nx < n and 0 <= ny < n and board[ny][nx] == 0:
                 break
     return lst
 
-def reverse(lst, color):
+def reverse(y, x, lst, color):
     for i in lst:
+        lx, ly = x, y
         ny, nx, value = i
-        if value == 0:
-            for q in range(ny+1, n):
-                board[ny+q][nx] = color
-        elif value == 1:
-            for q in range(ny+1, y):
-                for w in range(x+1, nx):
-                    if q == -(w):
-                        board[ny+q][x+w] = color
-        elif value == 2:
-            for w in range(x+1, nx):
-                board[ny][x+w] = color
-        elif value == 3:
-            for q in range(y+1, ny):
-                for w in range(x+1, nx):
-                    if q == w:
-                        board[y+q][x+w] = color
-        elif value == 4:
-            for q in range(y+1, ny):
-                board[y+q][nx] = color
-        elif value == 5:
-            for q in range(y+1, ny):
-                for w in range(nx+1, x):
-                    if q == -(w):
-                        board[y+q][nx+w] = color
-        elif value == 6:
-            for w in range(nx+1, x):
-                board[ny][nx+w] = color
-        elif value == 7:
-            for q in range(ny+1, y):
-                for w in range(nx+1, x):
-                    if q == w:
-                        board[ny+q][nx+w] = color
+        while not(ly == ny and lx == nx):
+            ly = ly + dy[value]
+            lx = lx + dx[value]
+            board[ly][lx] = color
 test = int(input())
 dx = [0, 1, 1, 1, 0, -1, -1, -1]
 dy = [-1, -1, 0, 1, 1, 1, 0, -1]
@@ -70,15 +44,11 @@ for t in range(1, test+1):
         if color == 1:
             board[y][x] = 1
             lst = go(y, x, color, 2)
-            reverse(lst, 1)
+            reverse(y, x, lst, 1)
         else:
             board[y][x] = 2
             lst = go(y, x, color, 1)
-            reverse(lst, 2)
-        print(lst)
-        for k in board:
-            print(k)
-        print('-----')
+            reverse(y, x, lst, 2)
     b_cnt = 0
     w_cnt = 0
     for i in range(n):
@@ -87,5 +57,4 @@ for t in range(1, test+1):
                 b_cnt += 1
             elif board[i][j] == 2:
                 w_cnt += 1
-    
     print(f'#{t} {b_cnt} {w_cnt}')
