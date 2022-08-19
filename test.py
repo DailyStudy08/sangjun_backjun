a30e43d31bd2289e8ab0ff2a653d5fec1ffea577
BaeSangjun <qotkdwns1324@naver.com>
2022-08-19 17:02:45 +0900

    11660

--- test.py
+++ test.py
@@ -1,60 +1,25 @@
 import sys
 sys.stdin = open('input.txt')
-input = sys.stdin.readline
 
-def go(my, mx, color, no_color):
-    lst = []
-    for i in range(8):
-        nx = mx
-        ny = my
-        for _ in range(n):
-            nx += dx[i]
-            ny += dy[i]
-            if 0 <= nx < n and 0 <= ny < n and board[ny][nx] == color:
-                lst.append((ny, nx, i))
-                break
-            elif 0 <= nx < n and 0 <= ny < n and board[ny][nx] == no_color:
-                continue
-            elif 0 <= nx < n and 0 <= ny < n and board[ny][nx] == 0:
-                break
-    return lst
+def dfs(start, graph, visit):
+    stack = graph[start]
+    while stack:
+        v = stack.pop()
+        if v == 99:
+            return 1
+        visit[v] = True
+        for i in graph[v]:
+            if not visit[i]:
+                stack.append(i)
+    return 0
+        
 
-def reverse(y, x, lst, color):
-    for i in lst:
-        lx, ly = x, y
-        ny, nx, value = i
-        while not(ly == ny and lx == nx):
-            ly = ly + dy[value]
-            lx = lx + dx[value]
-            board[ly][lx] = color
-test = int(input())
-dx = [0, 1, 1, 1, 0, -1, -1, -1]
-dy = [-1, -1, 0, 1, 1, 1, 0, -1]
-for t in range(1, test+1):
-    n, m = map(int, input().split())
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    board[n//2][n//2] = 2
-    board[n//2-1][n//2-1] = 2
-    board[n//2-1][n//2] = 1
-    board[n//2][n//2-1] = 1
-    for _ in range(m):
-        y, x, color = map(int, input().split())
-        y -= 1
-        x -= 1
-        if color == 1:
-            board[y][x] = 1
-            lst = go(y, x, color, 2)
-            reverse(y, x, lst, 1)
-        else:
-            board[y][x] = 2
-            lst = go(y, x, color, 1)
-            reverse(y, x, lst, 2)
-    b_cnt = 0
-    w_cnt = 0
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == 1:
-                b_cnt += 1
-            elif board[i][j] == 2:
-                w_cnt += 1
-    print(f'#{t} {b_cnt} {w_cnt}')
+for t in range(1, 11):
+    test, line = map(int, input().split())
+    graph = [[] for _ in range(100)]
+    visit = [False for _ in range(100)]
+    lst = list(map(int, input().split()))
+    for i in range(0, len(lst), 2):
+        graph[lst[i]].append(lst[i+1])
+    print(graph)
+    print(f'#{t} {dfs(0, graph, visit)}')
